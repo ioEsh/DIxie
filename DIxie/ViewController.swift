43d10101102abd600d34ae96e8b1f781f38d8aa8
Grey Grissom <grey.grissom@aya.yale.edu>
2018-03-13 00:17:33 -0700

    Initial Commit

---
+++ DIxie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DIxie
+//
+//  Created by Grey Grissom on 3/13/18.
+//  Copyright © 2018 Grey Grissom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
